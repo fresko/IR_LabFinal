adf0f8647c85fa6638b57b0d6b664e887e696308
jcph <juan.paz.h@gmail.com>
2017-05-14 20:08:40 -0500

    se agrega el metodo de generacion de matrix de distancias

--- src/main/java/com/usb/ir/main/IR_CArray.java
+++ src/main/java/com/usb/ir/main/IR_CArray.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.usb.ir.ca;
+package com.usb.ir.main;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -32,6 +32,8 @@ public class IR_CArray {
             IR_CArray fileCa = new IR_CArray();
             //se convierte archivo Carray a matrix en memoria
             fileCa.FileToMatrix(DIR_CA, " ").forEach(System.out::println);
+            //se genera la matrix de distancias 
+            fileCa.maztrixDist(fileCa.FileToMatrix(DIR_CA, " ")).forEach(System.out::println);
 
         } catch (IOException ex) {
             Logger.getLogger(IR_CArray.class.getName()).log(Level.SEVERE, null, ex);
@@ -87,25 +89,65 @@ public class IR_CArray {
         return lstCA;
 
     }
-    
+
     /**
      * Metodo que calcula el centroide del cluster
-     * @param mi  cantidad de veces que se repeti el cluster en el grupo ca (fila de ca)
-     * @param sumhit sumatorias del peso de cluster en el grupo ca (fila de ca)
-     * @return 
+     *
+     * @param mi cantidad de veces que se repeti el cluster en el grupo ca (fila
+     * de ca)
+     * @param sumhit sumatorias del peso o score de cluster en el grupo ca (fila
+     * de ca)
+     * @return
      */
-    public double centroid(int mi,double sumhit){
-        return !(mi == 0)? 1/mi * sumhit : 0;
+    static public double centroid(int mi, double sumhit) {
+        return !(mi == 0) ? 1 / mi * sumhit : 0;
     }
-    
+
     /**
-     * Metodo que calcula el la distancia 
-     * @param centroid 
+     * Metodo que calcula el la distancia
+     *
+     * @param centroid
      * @param cluster cluster del ca
-     * @return 
+     * @return
      */
-    public double dist(double centroid,int cluster){
-      return  Math.sqrt(Math.pow(2, centroid) + Math.pow(2,cluster));
+    static public double dist(double centroid, int cluster) {
+        return Math.sqrt(Math.pow(2, centroid) + Math.pow(2, cluster));
+    }
+
+    /**
+     * Metodo que genera la matrix de distancias
+     *
+     * @param lstCa
+     * @return
+     */
+    public List<double[]> maztrixDist(List<char[]> lstCa/*,List<Document> lstDoc*/) {
+        char[] vectCluster = {'0', '1', '2', '3', '4'}; //es necesario no sacarlo de la lista (para deuda tecnica)
+        double[] vectPeso = {20, 30, 40, 50, 10, 15, 16, 17, 31, 40, 11, 12, 13, 14, 22, 23, 24, 25, 19, 32, 33}; //es necesario no sacarlo de los Documentos (para deuda tecnica)
+        List<double[]> maztrixDist = new ArrayList<double[]>();
+        double[] vectDist = new double[vectPeso.length];
+
+        for (int ca = 0; ca < lstCa.size(); ca++) {
+            int indexDist = 0;
+            for (int i = 0; i < vectCluster.length; i++) {
+                int mi = 0;
+                double sumhit = 0;
+                for (int j = 0; j < lstCa.get(ca).length; j++) {
+                    if (vectCluster[i] == lstCa.get(ca)[j]) {
+                        mi++;
+                        sumhit = sumhit + vectPeso[j];
+                    }
+                    if (j == lstCa.get(ca).length - 1) {
+                        vectDist[indexDist] = dist(centroid(mi, sumhit), vectCluster[i]);
+                        indexDist++;
+                    }
+                }
+                if (i == vectCluster.length - 1) {
+                    maztrixDist.add(ca, vectDist);
+                }
+            }
+
+        }
+        return maztrixDist;
     }
 
 }
